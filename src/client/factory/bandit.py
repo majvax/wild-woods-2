916a261d2991b4f11a1930681c58323f74cca94f
majvax <majvax.contact@gmail.com>
2026-03-15 00:08:54 +0100

    refactor: better demo that now spawn bandits that don't have any logic. Purpose/10?

--- src/client/factory/bandit.py
+++ src/client/factory/bandit.py
@@ -1,8 +1,12 @@
 import esper
+import pygame
 
+from client.component.gameplay import Sprite
 from client.component.physics import Position, Speed, Velocity
 from client.component.tags import EnemyTag
 
 
 def create_bandit(pos: Position):
-    esper.create_entity(pos, Velocity(0, 0), Speed(300), EnemyTag())
+    # TODO: replace with actual bandit sprite
+    surface = pygame.image.load("sprite/player/standard/hurt/up/6.png").convert_alpha()
+    esper.create_entity(pos, Velocity(0, 0), Speed(200), Sprite(surface), EnemyTag())
