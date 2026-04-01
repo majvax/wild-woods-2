a6b302dd745d4e381496cc508c12e2cb325a945f
Zecroww <enzo.bal06@gmail.com>
2026-04-01 14:26:16 +0200

    fix: fixed typing in module 'noise' and fixed conf file

---
+++ typings/noise.pyi
@@ -0,0 +1,10 @@
+def pnoise2(
+    x: float, 
+    y: float, 
+    octaves: int = ..., 
+    persistence: float = ..., 
+    lacunarity: float = ..., 
+    repeatx: int = ..., 
+    repeaty: int = ..., 
+    base: int = ...
+) -> float: ...
