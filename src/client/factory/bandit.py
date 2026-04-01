28223b0043075b1647b6dd168a9adf18f60ff9b2
majvax <majvax.contact@gmail.com>
2026-04-01 12:44:47 +0200

    feat: added the modification to the bandit factory

--- src/client/factory/bandit.py
+++ src/client/factory/bandit.py
@@ -1,12 +1,24 @@
 import esper
 import pygame
 
+from client.component.ai import AI, PatrolRuntime, PatrolSettings
 from client.component.gameplay import Sprite
 from client.component.physics import Position, Speed, Velocity
 from client.component.tags import EnemyTag
+from client.component.targeting import Targeting
 
 
 def create_bandit(pos: Position):
     # TODO: replace with actual bandit sprite
     surface = pygame.image.load("sprite/player/standard/hurt/up/6.png").convert_alpha()
-    esper.create_entity(pos, Velocity(0, 0), Speed(200), Sprite(surface), EnemyTag())
+    esper.create_entity(
+        pos,
+        Velocity(0, 0),
+        Speed(200),
+        Sprite(surface),
+        EnemyTag(),
+        AI(),
+        Targeting(range=200, atk_range=50),
+        PatrolSettings.from_random(),
+        PatrolRuntime(),
+    )
