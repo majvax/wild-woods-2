2871eb85c21b14fb90724a231934e0641d4086fc
gogow <hugo.viard73@gmail.com>
2026-05-22 10:21:53 +0200

    update : possibilité d'entendre la musique

--- src/client/scene/game.py
+++ src/client/scene/game.py
@@ -37,6 +37,8 @@ class GameScene(Scene):
 
     @override
     def on_enter(self) -> None:
+        pygame.mixer.music.load("music/main-music.mp3")
+        pygame.mixer.music.play(-1)
         esper.add_processor(InputProc())
         esper.add_processor(TargetingProc())
         esper.add_processor(BrainProc())
