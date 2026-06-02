b2fb1f6665edee7a390af0c4003c5423e269deae
majvax <majvax.contact@gmail.com>
2026-06-02 11:57:09 +0200

    refactor: some stuff

--- src/client/factory/item.py
+++ src/client/factory/item.py
@@ -1,7 +1,7 @@
 import esper
 import pygame
 
-from client.component import ItemKind, ItemTag, Position, Sprite
+from client.component import Hitbox, ItemKind, ItemTag, Position, Sprite
 
 ITEM_SPRITE_PATHS: dict[ItemKind, str] = {
     ItemKind.GOLD: "assets/sprite/icons/coin.png",
@@ -15,4 +15,9 @@ _ITEM_DROP_SIZE = 20
 def create_item(pos: Position, kind: ItemKind) -> None:
     raw = pygame.image.load(ITEM_SPRITE_PATHS[kind]).convert_alpha()
     surface = pygame.transform.scale(raw, (_ITEM_DROP_SIZE, _ITEM_DROP_SIZE))
-    esper.create_entity(Position(pos.x, pos.y), Sprite(surface), ItemTag(kind))
+    esper.create_entity(
+        Position(pos.x, pos.y),
+        Sprite(surface),
+        ItemTag(kind),
+        Hitbox(width=_ITEM_DROP_SIZE, height=_ITEM_DROP_SIZE),
+    )
