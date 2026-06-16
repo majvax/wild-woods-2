b8f28450f527f566696557fc1a7cf0e656daa609
majvax <majvax.contact@gmail.com>
2026-06-16 16:19:57 +0200

    fix: changed map path

--- src/client/core/background.py
+++ src/client/core/background.py
@@ -11,7 +11,7 @@ CHUNK_SIZE_TILES = 32
 def get_bg_data() -> tuple[pygame.Surface, int, int]:
     global _bg_image, _bg_w, _bg_h
     if _bg_image is None:
-        _bg_image = pygame.image.load("map/background.png").convert()
+        _bg_image = pygame.image.load("assets/map/background.png").convert()
         _bg_w = _bg_image.get_width()
         _bg_h = _bg_image.get_height()
 
